d463c8d3177d065a52d7e028146df1fe68a7963f
dimak911 <dimak911@gmail.com>
2022-04-03 13:17:16 +0000

    Deploying to gh-pages from @ dimak911/team-project-1@dad6095739d422662b39c80785411dcc6d1e1caf 🚀

--- src.1e5a2c69.js
+++ src.1e5a2c69.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-project-1/src.031e0502.js.map
+//# sourceMappingURL=/team-project-1/src.1e5a2c69.js.map
